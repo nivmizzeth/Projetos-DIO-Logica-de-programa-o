2b4fc38fd541fede0ecc9f643dc0232fafe4bbce
Léo Marques <nivmizzeth@gmail.com>
2026-01-04 19:04:47 -0300

    Treimamento funções

---
+++ index.js
@@ -0,0 +1,10 @@
+function pares(numero){
+    
+    if (numero % 2 === 0){
+        console.log(numero + " Este número é par.")
+    } else {
+        console.log(numero + " Este número é impar.")
+    }
+}
+
+pares(98)
